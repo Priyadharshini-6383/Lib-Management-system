68a618ed649482d13d5c5ac0043bf64c724e65b0
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-06 13:12:15 +0530

    Running server.js at port 5000

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/" , (req , res ) => {
+res.send(" Welcome to Library Management System");
+});
+
+app.listen(PORT , () => {
+    console.log(`The server will be run on http://localhost:${PORT}`);
+});
