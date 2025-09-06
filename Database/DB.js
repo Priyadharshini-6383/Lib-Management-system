e4525922cbf63e0d609bf8df5ee8299eda850321
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-06 13:56:39 +0530

    ignoring .env

--- Database/DB.js
+++ Database/DB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectdB = async () =>{
 try {
     
-    await mongoose.connect("mongodb+srv://Priyadharshini:rqwDjFcCZa3ncgxw@cluster0.xtnbh5o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGODB_URI);
 
 
     console.log("Database connected Successfully ....");
