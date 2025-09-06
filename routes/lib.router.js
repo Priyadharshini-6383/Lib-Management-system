e22ae9880bd2711b2c0e447c8cc569e34e85d50c
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-06 13:16:07 +0530

    Importing router and adding get , post , put , commit

---
+++ routes/lib.router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/" ,BooksRead);
+
+router.post("/",BooksCreate);
+
+router.put("/:id" , BooksUpdate);
+
+router.delete("/:id" , BooksDelete);
